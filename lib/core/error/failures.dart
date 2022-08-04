e0f34a0b4c3b571038e05719ed0627131902152e
Pualoo <phchirnev@hotmail.com>
2022-08-04 03:15:19 -0300

    feat: 🎸 Implement failure and exeption abstract classes

---
+++ lib/core/error/failures.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+abstract class Failure extends Equatable {
+  @override
+  List<Object> get props => [];
+}
+
+//General failures
+
+class NetworkFailure extends Failure {}
+
+class ServerFailure extends Failure {}
+
+class CacheFailure extends Failure {}
